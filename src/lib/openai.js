45714089b43156ef3adc33d667fed5c4b6a08abd
Damian Soto <soto.platero@gmail.com>
2024-09-15 08:18:27 -0400

    update

---
+++ src/lib/openai.js
@@ -0,0 +1,17 @@
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+
+export async function chat(input, system) {
+    
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4o-mini',
+      messages: [
+        { role: 'system', content: system},
+        { role: 'user', content: input }
+      ],
+    });
+
+    const content = response?.choices[0]?.message?.content?.trim() ?? '[]';
+    // console.log(content)
+    return JSON.parse(content);
+  }
